79af9129a8999f686e6d4a74ee93a7b8b00c207b
Ahmed Abdien <ahmed.g.abdien@gmail.com>
2023-08-12 05:39:40 +0300

    manual smoke

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,6 +1,7 @@
-import { useState } from 'react'
-import { Html, Line, OrbitControls, Scroll, ScrollControls, Text, useScroll } from '@react-three/drei'
-import { useFrame, useThree } from '@react-three/fiber'
+import * as THREE from 'three'
+import { useRef, useState, createRef, forwardRef } from 'react'
+import { Box, Html, Line, OrbitControls, Scroll, ScrollControls, Text, useScroll } from '@react-three/drei'
+import { useFrame, useLoader, useThree } from '@react-three/fiber'
 import { useControls } from 'leva'
 import { Perf } from 'r3f-perf'
 import IntroPage from './components/IntroPage'
@@ -15,17 +16,72 @@ export default function Experience() {
 
         {/* <Perf position="top-left" /> */}
 
-        {/* <OrbitControls makeDefault /> */}
+        <OrbitControls makeDefault />
 
-        <ScrollControls damping={0} pages={pagesNum} >
+        {/* <ScrollControls damping={0} pages={pagesNum} >
             <Scroll>
                 <Pages pagesNum={pagesNum} />
             </Scroll>
-        </ScrollControls>
+        </ScrollControls> */}
 
-        {/* <axesHelper args={[5]} /> */}
+        <Smokey />
+        <Smoke />
+        <directionalLight color="#fff" intensity={0.3} position={[-1, 0, 1]} />
+
+        <axesHelper args={[5]} />
     </>
 }
+
+const Smokey = forwardRef((props, ref) => {
+
+    const url = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/95637/Smoke-Element.png'
+    const smokeTexture = useLoader(THREE.TextureLoader, url)
+    console.log("j");
+    return <>
+        <mesh rot ref={ref} {...props}>
+            <planeGeometry args={[2.5, 2.5]} />
+            <meshLambertMaterial color={'#00dddd'} map={smokeTexture} transparent />
+        </mesh>
+    </>
+})
+
+const Smoke = () => {
+    const count = 150
+    const refs = useRef(
+        Array.from({ length: count }).map(() => createRef())
+    );
+
+    useFrame((state, delta) => {
+        refs.current.forEach((ref, index) => {
+            // console.log("hi");
+            ref.current.rotation.z += (delta * 0.2)
+        })
+
+    })
+
+    return <>
+        <group scale={1}>
+            {
+                refs.current.map((ref, index) => {
+                    console.log("j");
+                    return < Smokey
+                        ref={ref}
+                        key={index}
+                        position={
+                            [
+                                Math.random() * 4 - 2,
+                                Math.random() * 4 - 2,
+                                Math.random() * 10 - 1
+                            ]}
+                        rotation={[0, 0, Math.random() * 360]}
+                    />
+                })
+            }
+        </group>
+    </>
+}
+
+
 
 const Pages = ({ pagesNum }) => {
     const data = useScroll()
